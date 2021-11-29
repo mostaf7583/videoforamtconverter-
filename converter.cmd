5d794d6b05d02e9331f3e725eee3bfc05cdc8d75
mostaf7583 <73074293+mostaf7583@users.noreply.github.com>
2021-11-29 21:51:58 +0200

    explain

--- converter.cmd
+++ converter.cmd
@@ -1,10 +1,7 @@
 @echo off
- set "C:\Users\mosta\Videos\Digital System Design\Vod\tut"
-mkdir conv
- set "C:\Users\mosta\Videos\Digital System Design\Vod\conv"
-
+ set "path of the wanted to convert vidos"
+ mkdir conv 
  PUSHD "%sourcedir%"
-
  for %%F in (*.ts) DO ffmpeg -i "%%F" -acodec copy -vcodec copy "conv/%%F.mp4"
 
  POPD
